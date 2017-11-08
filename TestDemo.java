0224a60bf67b1125fa8963414bc8d5fdc3aae91c
Sean Myung <sungwook.myung@gmail.com>
2017-11-08 16:25:23 -0500

    added Test Program

---
+++ TestDemo.java
@@ -0,0 +1,17 @@
+package test;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] myList = {1,2,3,4};
+		
+		for(int i=0; i< myList.length; i++) {
+			System.out.println(myList[i]);
+		}
+		
+
+	}
+
+}
